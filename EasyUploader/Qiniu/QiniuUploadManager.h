fc0091973068f77f1946a5a8712ff95b5f2e8394
sytuzhouyong <308872563@qq.com>
2017-04-07 00:01:01 +0800

    七牛管理类创建,待实现上传 token 算法

---
+++ EasyUploader/Qiniu/QiniuUploadManager.h
@@ -0,0 +1,13 @@
+//
+//  QiniuUploadManager.h
+//  EasyUploader
+//
+//  Created by zhouyong on 17/4/6.
+//  Copyright © 2017年 zhouyong. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface QiniuUploadManager : NSObject
+
+@end
---
+++ EasyUploader/Qiniu/QiniuUploadManager.m
@@ -0,0 +1,15 @@
+//
+//  QiniuUploadManager.m
+//  EasyUploader
+//
+//  Created by zhouyong on 17/4/6.
+//  Copyright © 2017年 zhouyong. All rights reserved.
+//
+
+#import "QiniuUploadManager.h"
+
+@implementation QiniuUploadManager
+
+
+
+@end
