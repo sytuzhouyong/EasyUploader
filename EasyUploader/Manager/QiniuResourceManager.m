d0e590356db87407bb60e403ebffb32862a2b3db
sytuzhouyong <308872563@qq.com>
2017-07-17 23:52:18 +0800

    add resource list request interface

--- EasyUploader/Manager/QiniuResourceManager.m
+++ EasyUploader/Manager/QiniuResourceManager.m
@@ -8,11 +8,28 @@
 
 #import "QiniuResourceManager.h"
 
+#define kHttpManager    [AFHTTPSessionManager manager]
 
 @implementation QiniuResourceManager
 
 + (void)queryResourcesWithPrefix:(NSString *)prefix limit:(int)limit offset:(int)offset {
+    NSString *url = [NSString stringWithFormat:@"%@/list", kQiniuResourceHost];
+    NSDictionary *parma = @{@"bucket": kBucket,
+                            @"marker": @(offset),
+                            @"limit": @(limit),
+                            @"prefix": prefix,
+                            @"delimiter": @""
+                            };
+    [kHttpManager POST:url parameters:parma constructingBodyWithBlock:^(id<AFMultipartFormData>  _Nonnull formData) {
+        ;
+    } progress:^(NSProgress * _Nonnull uploadProgress) {
 
+    } success:^(NSURLSessionDataTask * _Nonnull task, id _Nullable responseObject) {
+        NSLog(@"response = %@", responseObject);
+
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"error = %@", error);
+    }];
 }
 
 @end
