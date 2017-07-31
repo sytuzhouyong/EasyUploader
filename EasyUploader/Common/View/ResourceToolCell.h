81adacc416bd2bdac8bff6fd2907f83123caa555
yong <308872563@qq.com>
2017-07-31 20:56:12 +0800

    Create ResourceToolCell.h

---
+++ EasyUploader/Common/View/ResourceToolCell.h
@@ -0,0 +1,18 @@
+//
+//  ResourceToolCell.h
+//  EasyUploader
+//
+//  Created by zhouyong on 17/7/31.
+//  Copyright © 2017年 zhouyong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "ToolCell.h"
+
+@interface ResourceToolCell : ToolCell
+
+@property (nonatomic, strong) UILabel *detailLabel;
+@property (nonatomic, strong) UILabel *sizeLabel;
+
+
+@end
